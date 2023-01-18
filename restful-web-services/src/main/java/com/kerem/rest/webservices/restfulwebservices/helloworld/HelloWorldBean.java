1d734946432fc4241e95a27023feadb28fa0bd2b
Kerem Ersan <keremersan01@gmail.com>
2023-01-18 11:37:08 +0300

    Basic HelloWorld REST APIs are written

---
+++ restful-web-services/src/main/java/com/kerem/rest/webservices/restfulwebservices/helloworld/HelloWorldBean.java
@@ -0,0 +1,24 @@
+package com.kerem.rest.webservices.restfulwebservices.helloworld;
+
+public class HelloWorldBean {
+	private String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorld [message=" + message + "]";
+	}
+
+}
---
+++ restful-web-services/src/main/java/com/kerem/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.kerem.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+//	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+//	public String helloWorld() {
+//		return "Hello World!";
+//	}
+	
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "Hello World!";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World Bean!");
+	}
+	
+	// Path Parameters
+	// /users/1/todos/100
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World!, %s", name));
+	}
+
+}
