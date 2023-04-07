bbb3ae0e4d8f7b7f132ff0a545a01b21ea2ea3de
keremersann <keremersan01@gmail.com>
2023-04-07 22:06:46 +0300

    async and await calls are added

--- todo-app-frontend/src/components/todo/security/AuthContext.js
+++ todo-app-frontend/src/components/todo/security/AuthContext.js
@@ -11,34 +11,39 @@ export default function AuthProvider({ children }) {
   // 2) put some state in the context
   const [isAuthenticated, setAuthenticated] = useState(false);
   const [username, setUsername] = useState(null);
+  const [token, setToken] = useState(null);
 
-  // function login(username, password){
-  //     if(username === 'kerem' && password === '123'){
-  //         setAuthenticated(true)
-  //         setUsername("kerem")
-  //         return true
-  //     }else{
-  //         setAuthenticated(false)
-  //         return false
-  //     }
-  // }
-
-  function login(username, password) {
+  async function login(username, password) {
     const baToken = "Basic " + window.btoa(username + ":" + password);
 
-    executeBasicAuthenticationService(baToken)
-      .then((response) => console.log(response))
-      .catch((error) => console.log(error));
-
-    setAuthenticated(false);
+    try {
+      const response = await executeBasicAuthenticationService(baToken);
+      if (response.status === 200) {
+        setAuthenticated(true);
+        setUsername(username);
+        setToken(baToken);
+        return true;
+      } else {
+        logout();
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      logout();
+      return false;
+    }
   }
 
   function logout() {
     setAuthenticated(false);
+    setUsername(null);
+    setToken(null);
   }
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout, username }}>
+    <AuthContext.Provider
+      value={{ isAuthenticated, login, logout, username, token }}
+    >
       {children}
     </AuthContext.Provider>
   );
