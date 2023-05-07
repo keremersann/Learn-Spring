99a1d04fcfa6986fc93a97f63718f7dbb98003e0
keremersann <keremersan01@gmail.com>
2023-05-07 13:16:15 +0300

    Jwt encoder is added

---
+++ learn-spring-security/src/main/java/com/kerem/learnspringsecurity/security/jwt/JwtAuthenticationResource.java
@@ -0,0 +1,48 @@
+package com.kerem.learnspringsecurity.security.jwt;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.util.stream.Collectors;
+
+@RestController
+public class JwtAuthenticationResource {
+
+    private JwtEncoder jwtEncoder;
+
+    public JwtAuthenticationResource(JwtEncoder jwtEncoder){
+        this.jwtEncoder = jwtEncoder;
+    }
+
+    @PostMapping("/authentication")
+    public JwtResponse jwtAuthenticate(Authentication authentication){
+        return new JwtResponse(createToken(authentication));
+    }
+
+    private String createToken(Authentication authentication) {
+        var claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 30))
+                .subject(authentication.getName())
+                .claim("scope",createScope(authentication))
+                .build();
+
+        JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(claims);
+        return jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+    }
+
+    private String createScope(Authentication authentication) {
+        return authentication.getAuthorities()
+                .stream().map(a -> a.getAuthority())
+                .collect(Collectors.joining(" "));
+    }
+}
+
+record JwtResponse(String token) {}
